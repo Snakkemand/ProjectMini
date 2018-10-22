0d95c705fe8febb1dbf7bdc10a5da0912a4b2dfa
Gustav Jakobsen <fe12gusjak@gmail.com>
2018-10-22 13:15:19 +0200

    client changes

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -5,7 +5,21 @@ import java.awt.*;
 
 public class ChatClient extends Applet
 {
-    private Socket socket
+    private Socket socket               = null;
+    private DataInputStream console     = null;
+    private DataOutputStream streamOut  = null;
+    private ChatClientThread client     = null;
+    private TextArea display = new TextArea();
+    private TextField input  = new TextField();
+    private Button send      = new Button("Send"),  connect = new Button("Connect"),
+                   quit      = new Button("Bye");
+    private String  serverName = "localhost";
+    private int     serverPort = 6969;
 
-    
+    public void init()
+    {
+
+
+
+    }
 }
