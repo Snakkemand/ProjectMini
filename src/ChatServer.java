5f16ab3ea517d4cfef1f3606cfc447453ce78349
Snakkemand <43171290+Snakkemand@users.noreply.github.com>
2018-10-22 12:50:40 +0200

    Changed names and added privates to server

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,7 +1,13 @@
+import java.net.*;
+import java.io.*;
+
 public class ChatServer {
 
     public static void main(String args[]) {
 
+        private Socket socket = null;
+        private ServerSocket server=null;
+        private DataInputStream streamIn=null;
 
 
     }
