5bbc520347a12f24e3a9aaa20a31588846619f6d
sunnybear1230 <slapingjack@gmail.com>
2018-10-23 11:01:00 +0200

    chatClientThread done

--- src/ChatClientThread.java
+++ src/ChatClientThread.java
@@ -1,2 +1,43 @@
-public class ChatClientThread {
+import java.net.*;
+import java.io.*;
+
+public class ChatClientThread extends Thread{
+    private Socket socket = null;
+    Private chatClient client = null;
+    private DataInputStream streamIn = null;
+
+    Public ChatClientThread(ChatClient _client, _Socket socket) {
+        client = _client;
+        socket = _socket;
+        open();
+        start();
+    }
+
+    public void open() {
+        try {
+            streamIn = new DataInputStream(socket.getInputStream());
+        }
+        catch(IOExeption ioe) {
+            System.out.println("Error getting input: " + ioe );
+            client.stop();
+        }
+    }
+    public void close() {
+        try {
+            if (stramIn != null) streamIn.Close();
+        }
+        catch(IOExeption ioe)
+        { system.out.println("Error closing input stream: " + ioe);
+        }
+    }
+    public void run() {
+        while (true) {
+            try {
+                client.handle(streamIn.readUTF());
+            }
+            catch(IOExeption ioe) {
+                system.out.println("listening error:" + ioe.getMessage());
+            }
+        }
+    }
 }
